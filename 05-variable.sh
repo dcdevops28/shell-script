ad4b44e8858212c5934c71fa9e7b4c2279ed49f0
guffajaffa <chandrasekkhard1990@gmail.com>
2024-04-06 17:00:02 +0530

    added 2nd script

--- 05-variable.sh
+++ 05-variable.sh
@@ -4,4 +4,4 @@ echo "Please enter your username"
 
 read USERNAME #the value entered above will be automatically attached to USERNAME variable
 
-echo "username is: $USERNAME, password is: $PASSWORD"
+echo "username is: $USERNAME"
