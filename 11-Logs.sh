45da2595abd94ef1ddc9b7fdaf0ce16f39b8c2be
guffajaffa <chandrasekkhard1990@gmail.com>
2024-04-07 13:41:27 +0530

    added 2nd script

---
+++ 11-Logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit1
+    else
+        echo "$2 ... SUCCESS"
+    fi   
+}
+
+    if [ $ID -ne 0 ]
+    then 
+        echo "Error:: please run this script with root access"
+        exit 1 # you can give other than 0
+    else
+        echo "Yor are root user"
+    fi # fi means reverce of if, indicating condition end
+
+    yum install mysql -y
+
+    VALIDATE $? "Installing MYSQL"
+
+    yum install git -y
+
+    VALIDATE $? "installing GIT"
